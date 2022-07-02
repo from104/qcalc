a00a79d053d7f73eb7a8f688a5fca4897f43e29e
Seo Kihyun <from104@gmail.com>
2022-07-03 04:47:14 +0900

    툴팀 컴포넌트

---
+++ src/components/MyTooltip.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts" />
+
+<template>
+  <q-tooltip class="text-dark bg-yellow text-body2 fa-border-all" anchor="top middle" self="bottom middle"
+    style="border: 1px solid black" :delay="500">
+    <slot />
+  </q-tooltip>
+</template>
