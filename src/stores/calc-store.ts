a35ff23eb2535c70ef90d2c09e5fee6df3403540
Seo Kihyun <from104@gmail.com>
2022-07-04 00:38:44 +0900

    키바인딩 제거 방식 변경으로 state 삭제

--- src/stores/calc-store.ts
+++ src/stores/calc-store.ts
@@ -1,13 +1,10 @@
 import { defineStore } from 'pinia';
 import { Calculator } from '../Calculator';
-import tinykeys, { KeyBindingMap } from 'tinykeys';
-
 
 export const useCalcStore = defineStore('calc', {
   state: () => ({
     alwaysOnTop: false,
     calc: new Calculator(),
-    keybindingRemoveFromCalc: tinykeys(window, {} as KeyBindingMap),
   }),
   getters: {},
   actions: {},
