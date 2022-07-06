40f41c0923e9c24130420f4ab5efe41584ed0e4d
Seo Kihyun <from104@gmail.com>
2022-07-07 04:17:26 +0900

    소수점 자릿수, 천 단위 구분 상태 추가 관련 actions 추가

--- src/stores/calc-store.ts
+++ src/stores/calc-store.ts
@@ -5,8 +5,29 @@ export const useCalcStore = defineStore('calc', {
   state: () => ({
     alwaysOnTop: false,
     calc: new Calculator(),
+    useGrouping: true,
+    decimalPlaces: -2
   }),
   getters: {},
-  actions: {},
+  actions: {
+    toggleAlwaysOnTop () {
+      this.alwaysOnTop = !this.alwaysOnTop;
+    },
+    toggleUseGrouping () {
+      this.useGrouping = !this.useGrouping;
+    },
+    setDecimalPlaces (decimalPlaces: number) {
+      const allowValues = [-2, 0, 2, 4, 6];
+      if (allowValues.includes(decimalPlaces)) {
+        this.decimalPlaces = decimalPlaces;
+      }
+    },
+    incDecimalPlaces () {
+      this.setDecimalPlaces(this.decimalPlaces + 2);
+    },
+    decDecimalPlaces () {
+      this.setDecimalPlaces(this.decimalPlaces - 2);
+    }
+  },
   persist: true,
 });
