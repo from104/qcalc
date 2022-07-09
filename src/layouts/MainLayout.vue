bf7301daf5a9815f1e7a4e81147561512d20a004
Seo Kihyun <from104@gmail.com>
2022-07-09 16:47:54 +0900

    왼쪽 메뉴 관련 숏컷 추가

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
-import { version } from '../../package.json';
 import { onMounted, ref } from 'vue';
 import { useQuasar } from 'quasar';
-import tinykeys from 'tinykeys';
+import tinykeys, { KeyBindingMap } from 'tinykeys';
+import { useRouter } from 'vue-router';
+
+import { version } from '../../package.json';
+
 import { useCalcStore } from 'src/stores/calc-store';
 import PathRoute from 'components/PathRoute.vue';
+
+const router = useRouter();
 
 const calcStore = useCalcStore();
 
 const $q = useQuasar();
 
 const paths = [
-  { title: '계산기', caption: '간단한 계산기', icon: 'calculate', path: '/' },
-  { title: '도움말', caption: '기능과 사용법', icon: 'help', path: '/help' },
-  { title: '소개', caption: '앱에 대한 소개', icon: 'info', path: '/about' },
+  { title: '계산기', caption: '간단한 계산기 (Q)', icon: 'calculate', path: '/' },
+  { title: '도움말', caption: '기능과 사용법 (H)', icon: 'help', path: '/help' },
+  { title: '소개', caption: '앱에 대한 소개 (A)', icon: 'info', path: '/about' },
 ];
 
 const leftDrawerOpen = ref(false);
@@ -33,54 +38,61 @@ const toggleAlwaysOnTop = (byManual = false) => {
 };
 
 onMounted(() => {
+  const keyBindingMaps: KeyBindingMap = {};
+
+  type Shortcut = [string[], () => void][];
+
+  const shortcuts: Shortcut = [
+    [['t'], () => toggleAlwaysOnTop(true)],
+    [['F3', 'm'], () => toggleLeftDrawer()],
+    [['q'], () => router.push({ path: '/' })],
+    [['F1', 'h', '?'], () => router.push({ path: '/help' })],
+    [['a'], () => router.push({ path: '/about' })],
+  ];
+
   if ($q.platform.is.electron) {
-    window.myAPI.setAlwaysOnTop(useCalcStore().$state.alwaysOnTop);
+    window.myAPI.setAlwaysOnTop(calcStore.alwaysOnTop);
   }
-  tinykeys(window, {
-    t: () => {
-      toggleAlwaysOnTop(true); // 수동으로 토글
-    },
+
+  shortcuts.forEach((shortcut) => {
+    const [keys, handler] = shortcut;
+    keys.forEach((key) => {
+      keyBindingMaps[key] = handler;
+    });
   });
+
+  tinykeys(window, keyBindingMaps);
 });
 </script>
 
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-          @focusin="($event.target as HTMLInputElement).blur()"
-        />
+      <q-toolbar @focusin="($event.target as HTMLInputElement).blur()">
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
         <q-toolbar-title> 간단한 계산기 </q-toolbar-title>
-        <!-- <div>Quasar v{{ $q.version }}</div> -->
-        <!-- <div>{{ $q.screen.width+"x"+$q.screen.height }}</div> -->
         <q-toggle
+          v-if="$q.platform.is.electron"
           v-model="calcStore.alwaysOnTop"
-          label="항상 위"
+          label="항상 위 (T)"
           left-label
           keep-color
           color="info"
-          :disable="!$q.platform.is.electron"
           @click="toggleAlwaysOnTop()"
-          @focusin="($event.target as HTMLInputElement).blur()"
         />
+        <!-- :disable="!$q.platform.is.electron" -->
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label class="text-h5" header> 메뉴 (v {{ version }}) </q-item-label>
+        <q-item-label class="text-h5" header> 메뉴 (M) </q-item-label>
         <PathRoute v-for="path in paths" :key="path.title" v-bind="path" />
       </q-list>
+      <q-footer class="q-pa-sm"> 버전 : {{ version }} </q-footer>
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container style="padding-bottom: 0px">
       <router-view />
     </q-page-container>
   </q-layout>
