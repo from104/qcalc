98bfc00ed177b69b807c05168b285f69e2b17abc
Seo Kihyun <from104@gmail.com>
2022-07-20 00:32:13 +0900

    클래스 폴더 생성 및 파일 이동

--- quasar.config.js
+++ quasar.config.js
@@ -51,6 +51,10 @@ module.exports = configure(function( /* ctx */ ) {
 
       // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
       build: {
+        // import folder alias
+        alias: {
+          'classes': 'src/classes',
+        },
         target: {
           browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
           node: 'node16',
