0e8bd0c3e210ed62c28a1ab407d506308a80b293
Seo Kihyun <from104@gmail.com>
2022-07-25 03:29:33 +0900

    히스토리 기능 추가 버튼에 아이콘 사용 사칙연산자 활성화 시 아이콘으로 표시

--- src/pages/CalcPage.vue
+++ src/pages/CalcPage.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount, reactive, ref, watch } from 'vue';
+import { onMounted, onBeforeUnmount, reactive, ref, watch, computed } from 'vue';
 import tinykeys, { KeyBindingMap } from 'tinykeys';
 import { copyToClipboard, useQuasar } from 'quasar';
 
 import { useCalcStore } from 'stores/calc-store';
 import MyTooltip from 'components/MyTooltip.vue';
+import type { History } from 'classes/Calculator';
 
 // 스토어 가져오기
 const calcStore = useCalcStore();
@@ -24,13 +25,13 @@ const localeOptions: Intl.NumberFormatOptions = reactive({
 });
 
 // 쉼표 표시를 위한 옵션을 설정하는 함수
-function setUseGrouping() {
+function setUseGrouping () {
   localeOptions.useGrouping = calcStore.useGrouping;
 }
 
 // 소수점 표시를 위한 옵션을 설정하는 함수
 // 인자가 있으면 인자로 설정하고 없으면 스토어에서 가져온 값으로 설정한다.
-function setDecimalPlaces(decimalPlaces: number | undefined = undefined) {
+function setDecimalPlaces (decimalPlaces: number | undefined = undefined) {
   if (decimalPlaces !== undefined) {
     calcStore.setDecimalPlaces(decimalPlaces);
   }
@@ -44,6 +45,11 @@ function setDecimalPlaces(decimalPlaces: number | undefined = undefined) {
   }
 }
 
+// 숫자를 표준 로케일 문자열로 변환하는 함수
+function toLocale (value: number): string {
+  return value.toLocaleString(locale, localeOptions);
+}
+
 // 계산 결과를 표시하는 변수 선언
 const result = ref('');
 const resultElement = ref<HTMLDivElement | null>(null);
@@ -54,7 +60,7 @@ const operator = ref('');
 // 계산 결과가 길 경우 툴팁 표시 상태 변수 선언
 const needResultTooltip = ref(false);
 
-function setNeedResultTooltip() {
+function setNeedResultTooltip () {
   // 원래 결과 칸 길이
   const ow = resultElement.value?.offsetWidth as number;
   // 결과 문자열의 크기
@@ -65,22 +71,15 @@ function setNeedResultTooltip() {
 }
 
 // 계산 결과, 연산자, 툴팁을 갱신하는 함수
-function refreshDisplay() {
+function refreshDisplay () {
   // 소수점 고정이 아니고 결과에 수소점이 있으면 소수점 표시를 해야한다.
   if (calcStore.decimalPlaces === -2 && calc.getShownNumber().indexOf('.') !== -1) {
     const [integer, decimal] = calc.getShownNumber().split('.');
-    result.value = `${Number(integer).toLocaleString(locale, localeOptions)}.${decimal}`;
+    result.value = `${toLocale(Number(integer))}.${decimal}`;
   } else {
-    result.value = Number(calc.getShownNumber()).toLocaleString(locale, localeOptions);
+    result.value = toLocale(Number(calc.getShownNumber()));
   }
   operator.value = calc.getOperatorString() as string;
-
-  // 계산 결과가 칸에서 넘치면 툴팁으로 보이게 한다.
-  // 이렇게 지연시키지 않으면 툴팁 표시가 한 스텝 늦게 갱신됨
-  // TODO: setTimeout 을 사용하지 않는 방법을 찾아보자. 정상 작동은 하지만 불안하다.
-  // setTimeout(() => {
-  //   setNeedResultTooltip();
-  // }, 1);
 }
 
 // 두 변수를 감시하여 달라지면 결과를 갱신하여 표시
@@ -90,7 +89,7 @@ watch([calc, localeOptions], () => {
 
 const $q = useQuasar();
 
-function doCopy(): void {
+function doCopy (): void {
   copyToClipboard(result.value)
     .then(() => {
       $q.notify({
@@ -112,7 +111,7 @@ function doCopy(): void {
     });
 }
 
-function doPaste(): void {
+function doPaste (): void {
   navigator.clipboard
     .readText()
     .then((text) => {
@@ -134,17 +133,17 @@ function doPaste(): void {
     });
 }
 
-function toggleUseGrouping(): void {
+function toggleUseGrouping (): void {
   calcStore.toggleUseGrouping();
   setUseGrouping();
 }
 
-function incDecimalPlaces(): void {
+function incDecimalPlaces (): void {
   calcStore.incDecimalPlaces();
   setDecimalPlaces();
 }
 
-function decDecimalPlaces(): void {
+function decDecimalPlaces (): void {
   calcStore.decDecimalPlaces();
   setDecimalPlaces();
 }
@@ -195,6 +194,22 @@ const shortcuts: Shortcut = [
   [[','], toggleUseGrouping],
   [['['], decDecimalPlaces],
   [[']'], incDecimalPlaces],
+  [
+    ['h'],
+    () => {
+      if (!doDeleteHistory.value) {
+        showHistory.value = !showHistory.value;
+      }
+    },
+  ],
+  [
+    ['d'],
+    () => {
+      if (showHistory.value) {
+        doDeleteHistory.value = true;
+      }
+    },
+  ],
 ];
 
 // 계산기 키바인딩 제거하기위한 변수 선언
@@ -235,61 +250,46 @@ onMounted(() => {
 onBeforeUnmount(() => {
   keybindingRemoveAtUmount();
 });
+
+const showHistory = ref(false);
+
+const resultHistory = computed(() => (calc.getHistory() as unknown) as History[]);
+
+const doDeleteHistory = ref(false);
+
+const operatorIcon: { [key: string]: string } = {
+  '+': 'mdi-plus-box',
+  '-': 'mdi-minus-box',
+  '×': 'mdi-close-box',
+  '÷': 'mdi-division-box',
+}
 </script>
 
 <template>
-  <q-page id="qcalc" @focusin="($event.target as HTMLInputElement).blur()">
-    <q-card flat class="noselect row wrap q-px-md q-py-xs">
-      <q-card-section
-        class="col-9 row no-wrap items-center justify-start q-py-none q-px-xs"
-      >
-        <q-checkbox
-          v-model="calcStore.useGrouping"
-          label="쉼표: "
-          left-label
-          class="q-ml-sm"
-          @click="setUseGrouping()"
-        >
-          <my-tooltip>천 단위 구분 (,)</my-tooltip>
-        </q-checkbox>
+  <q-page id="qcalc" @focusin="($event.target as HTMLElement).blur()">
+    <q-card flat class="row wrap q-px-md q-py-xs">
+      <q-card-section class="noselect col-9 row no-wrap items-center justify-start q-py-none q-px-xs">
+        <q-checkbox v-model="calcStore.useGrouping" label="쉼표: " left-label class="q-ml-sm" @click="setUseGrouping()" />
         <div class="col-7 row no-wrap items-center">
           <my-tooltip>
-            소수점 고정값 선택 ('[',']')
-            <br />
             소수점 고정 상태:
             {{
-              calcStore.decimalPlaces == -2
-                ? '제한 없음'
-                : `${calcStore.decimalPlaces} 자리`
+                calcStore.decimalPlaces == -2
+                  ? '제한 없음'
+                  : `${calcStore.decimalPlaces} 자리`
             }}
           </my-tooltip>
           <div>소수점:</div>
-          <q-slider
-            v-model="calcStore.decimalPlaces"
-            :min="-2"
-            :step="2"
-            :max="6"
-            marker-labels
-            class="col-6 q-ml-md"
-            @change="setDecimalPlaces()"
-          >
+          <q-slider v-model="calcStore.decimalPlaces" :min="-2" :step="2" :max="6" marker-labels class="col-6 q-ml-md"
+            @change="setDecimalPlaces()">
             <template v-slot:marker-label-group="{ markerList }">
-              <div
-                class="cursor-pointer"
-                :class="(markerList[0] as any).classes"
-                :style="(markerList[0] as any).style"
-                @click="setDecimalPlaces((markerList[0] as any).value)"
-              >
+              <div class="cursor-pointer" :class="(markerList[0] as any).classes" :style="(markerList[0] as any).style"
+                @click="setDecimalPlaces((markerList[0] as any).value)">
                 x
               </div>
-              <div
-                v-for="val in [1, 2, 3, 4]"
-                :key="val"
-                class="cursor-pointer"
-                :class="(markerList[val] as any).classes"
-                :style="(markerList[val] as any).style"
-                @click="setDecimalPlaces((markerList[val] as any).value)"
-              >
+              <div v-for="val in [1, 2, 3, 4]" :key="val" class="cursor-pointer"
+                :class="(markerList[val] as any).classes" :style="(markerList[val] as any).style"
+                @click="setDecimalPlaces((markerList[val] as any).value)">
                 {{ (markerList[val] as any).value }}
               </div>
             </template>
@@ -297,45 +297,32 @@ onBeforeUnmount(() => {
         </div>
       </q-card-section>
 
-      <q-card-section class="col-3 row no-wrap justify-end q-py-none q-px-sm">
-        <q-btn class="q-pr-xs" flat v-if="operator" :label="operator">
+      <q-card-section class="noselect col-3 row no-wrap justify-end q-py-none q-px-sm">
+        <!-- <q-btn class="q-pr-xs" flat v-if="operator" :label="operator">
           <my-tooltip>현재 연산자</my-tooltip>
-        </q-btn>
-        <q-btn
-          flat
-          icon="content_copy"
-          color="primary"
-          class="q-ma-none q-pa-none q-pl-xs"
-          @click="doCopy"
-        >
+        </q-btn> -->
+        <q-btn flat icon="content_copy" color="primary" class="q-ma-none q-pa-none q-pl-xs" @click="doCopy">
           <my-tooltip>클릭하면 결과가 복사됩니다.</my-tooltip>
         </q-btn>
-        <q-btn
-          flat
-          icon="content_paste"
-          color="primary"
-          class="q-ma-none q-pa-none q-pl-xs q-pr-xs"
-          @click="doPaste"
-        >
+        <q-btn flat icon="content_paste" color="primary" class="q-ma-none q-pa-none q-pl-xs" @click="doPaste">
           <my-tooltip>클릭하면 숫자를 붙혀넣습니다.</my-tooltip>
+        </q-btn>
+        <q-btn flat icon="history" color="primary" class="q-ma-none q-pa-none q-pl-xs q-pr-xs"
+          @click="showHistory = true">
+          <my-tooltip>클릭하면 계산 결과 기록을 봅니다.</my-tooltip>
         </q-btn>
       </q-card-section>
       <q-card-section class="col-12 q-px-sm q-pt-none q-pb-sm">
-        <q-field
-          :model-value="result"
-          class="shadow-4"
-          filled
-          dense
-          :bg-color="needResultTooltip ? 'amber-2' : 'grey-2'"
-        >
+        <q-field :model-value="result" class="shadow-4 self-center" filled dense
+          :bg-color="needResultTooltip ? 'amber-2' : 'grey-2'">
+          <template v-slot:prepend v-if="operator != ''">
+            <div class="full-height q-mt-xs q-pt-xs">
+              <q-icon :name="operatorIcon[operator]" />
+            </div>
+          </template>
           <template v-slot:control>
-            <div
-              ref="resultElement"
-              v-mutation="setNeedResultTooltip"
-              v-mutation.characterData
-              class="self-center full-width no-outline text-h4 text-right"
-              style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis"
-            >
+            <div ref="resultElement" v-mutation="setNeedResultTooltip" v-mutation.characterData
+              class="self-center full-width no-outline ellipsis text-h4 text-right">
               {{ result }}
               <my-tooltip v-if="needResultTooltip">{{ result }}</my-tooltip>
             </div>
@@ -343,27 +330,77 @@ onBeforeUnmount(() => {
         </q-field>
       </q-card-section>
 
-      <q-card-section
-        class="col-3 q-pa-sm"
-        v-for="(button, index) in buttons"
-        :key="index"
-      >
-        <q-btn
-          class="glossy shadow-4 text-h5 full-width"
-          style="overflow: auto; min-height: 44px; max-height: 44px"
-          no-caps
-          :label="button[0].charAt(0) != '@' ? button[0] : undefined"
-          :icon="button[0].charAt(0) == '@' ? button[0].slice(1) : undefined"
-          :color="button[1]"
-          @click="button[3]"
-        />
+      <q-card-section class="noselect col-3 q-pa-sm" v-for="(button, index) in buttons" :key="index">
+        <q-btn class="glossy shadow-4 text-h5 full-width" style="overflow: auto; min-height: 44px; max-height: 44px"
+          no-caps :label="button[0].charAt(0) != '@' ? button[0] : undefined"
+          :icon="button[0].charAt(0) == '@' ? button[0].slice(1) : undefined" :color="button[1]" @click="button[3]" />
       </q-card-section>
     </q-card>
   </q-page>
+
+  <q-dialog v-model="showHistory" style="z-index: 10" position="bottom" transition-duration="300">
+    <q-bar dark class="noselect bg-primary text-white" @focusin="($event.target as HTMLElement).blur()">
+      <q-icon name="history" />
+      <div>계산 결과</div>
+
+      <q-space />
+
+      <q-btn dense flat icon="delete_outline" @click="doDeleteHistory = true" />
+      <!-- <q-btn dense flat icon="publish" /> -->
+      <q-btn dense flat icon="close" @click="showHistory = false" />
+    </q-bar>
+
+    <q-card id="history">
+      <q-card-section>
+        <q-list separator>
+          <q-item v-for="(item, index) in resultHistory" :key="index" class="text-right q-pa-sm">
+            <q-item-section>
+              <q-item-label>
+                {{
+                    ['+', '-', '×', '÷'].includes(item.operator) ?
+                      `${toLocale(item.preNumber)} ${item.operator} ${toLocale(item.argNumber as number)}`
+                      : ['%'].includes(item.operator) ?
+                        `${toLocale(item.preNumber)} / ${toLocale(item.argNumber as number)} * 100`
+                        : ['rec', 'pow2', 'sqrt'].includes(item.operator) ?
+                          `${item.operator} ( ${toLocale(item.preNumber)} )`
+                          : toLocale(item.preNumber)
+                }}
+              </q-item-label>
+              <q-item-label>
+                {{ `= ${toLocale(item.resultNumber)}` }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator />
+        </q-list>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+
+  <q-dialog v-model="doDeleteHistory" persistent transition-show="scale" transition-hide="scale" style="z-index: 15">
+    <q-card class="noselect bg-teal text-white" style="width: 200px">
+      <q-card-section> 계산 기록을 지우겠어요? </q-card-section>
+
+      <q-card-actions align="right" class="bg-white text-teal">
+        <q-btn flat label="아니" v-close-popup />
+        <q-btn flat label="ㅇㅇ" @click="calc.clearHistory()" autofocus v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <style scoped>
-.q-btn >>> .q-icon {
+.q-btn>>>.q-icon {
   font-size: 24px;
+}
+
+#history {
+  max-height: calc(100vh - 195px);
+  min-height: calc(100vh - 195px);
+  max-width: calc(100vw - 45px);
+}
+
+.q-bar {
+  max-width: calc(100vw - 45px);
 }
 </style>
