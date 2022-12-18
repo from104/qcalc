88b7525be9f6aea312c54f94deb212f811dff1c8
Seo Kihyun <from104@gmail.com>
2022-12-19 05:36:41 +0900

    단위 변환기 첫 작업

---
+++ src/pages/UnitPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import UnitPanelResult from 'components/UnitPanelResult.vue';
+import CalcButton from 'components/CalcButton.vue';
+import CalcHistory from 'components/CalcHistory.vue';
+</script>
+
+<template>
+  <q-page>
+    <q-card flat class="q-px-md q-pt-xs q-pb-md">
+      <UnitPanelResult />
+      <CalcButton />
+    </q-card>
+  </q-page>
+  <CalcHistory />
+</template>
