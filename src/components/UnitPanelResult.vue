af6691c039926dfede84f93dd4184e70cab03f11
Seo Kihyun <from104@gmail.com>
2022-12-24 19:59:50 +0900

    바로가기 키 추가 단위 바꾸기 추가 등

--- src/components/UnitPanelResult.vue
+++ src/components/UnitPanelResult.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted, onBeforeMount, onBeforeUnmount } from 'vue';
+import tinykeys, { KeyBindingMap } from 'tinykeys';
 
 import UnitConverter from 'classes/UnitConverter';
 
@@ -13,14 +14,21 @@ const store = useCalcStore();
 // 계산기 오브젝트를 스토어에서 가져오기 위한 변수 선언
 const calc = store.calc;
 
-// 최근 사용한 카테고리, 단위가 비어있거나 잘못된 값이면 초기화
-if (store.recentCategory == '') {
+// 단위 이름과 값을 바꾸기 위한 함수
+function swapUnitValue() {
+  calc.setShownNumber(unitResult.value);
+
+  const temp = store.recentUnitFrom[store.recentCategory];
+  store.recentUnitFrom[store.recentCategory] =
+    store.recentUnitTo[store.recentCategory];
+  store.recentUnitTo[store.recentCategory] = temp;
+}
+
+function initRecentCategoryAndUnit() {
   if (!UnitConverter.categories.includes(store.recentCategory)) {
     store.recentCategory = UnitConverter.categories[0];
   }
-}
 
-if (store.recentUnitFrom[store.recentCategory] == '') {
   if (
     !UnitConverter.getUnitLists(store.recentCategory).includes(
       store.recentUnitFrom[store.recentCategory]
@@ -30,9 +38,7 @@ if (store.recentUnitFrom[store.recentCategory] == '') {
       store.recentCategory
     )[0];
   }
-}
 
-if (store.recentUnitTo[store.recentCategory] == '') {
   if (
     !UnitConverter.getUnitLists(store.recentCategory).includes(
       store.recentUnitTo[store.recentCategory]
@@ -59,52 +65,149 @@ function setNeedUnitResultTooltip() {
 }
 
 const unitResult = computed(() => {
-  if (store.decimalPlaces == -2 && calc.getShownNumber().indexOf('.') !== -1) {
-    const [integer, decimal] = calc.getShownNumber().split('.');
-    return `${store.toLocale(Number(integer))}.${decimal}`;
-  } else {
-    return store.toLocale(Number(calc.getShownNumber()));
-  }
+  initRecentCategoryAndUnit();
+
+  return store.toLocale(
+    UnitConverter.convert(
+      store.recentCategory,
+      Number(calc.getShownNumber()),
+      store.recentUnitFrom[store.recentCategory],
+      store.recentUnitTo[store.recentCategory]
+    )
+  );
+});
+
+onBeforeMount(() => {
+  store.unitPanel = false;
+});
+
+// 계산기 키바인딩 제거하기위한 변수 선언
+let keybindingRemoveAtUmount = tinykeys(window, {} as KeyBindingMap);
+
+onMounted(() => {
+  initRecentCategoryAndUnit();
+
+  type Shortcut = [string[], () => void][];
+
+  const shortcuts: Shortcut = [
+    [['v'], () => store.unitPanelToggle()],
+    [['Shift+v'], () => (store.unitPanel ? swapUnitValue() : null)],
+  ];
+
+  // Support keyboard entry
+  const keyBindingMaps: KeyBindingMap = {};
+
+  shortcuts.forEach((shortcut) => {
+    const [keys, handler] = shortcut;
+    keys.forEach((key) => {
+      keyBindingMaps[key] = handler;
+    });
+  });
+
+  // 키바인딩하고 제거할 수 있는 메서드 백업;
+  keybindingRemoveAtUmount = tinykeys(window, keyBindingMaps);
+
+});
+
+onBeforeUnmount(() => {
+  store.unitPanel = false;
+
+  // dom 요소가 언마운트되기 전에 키바인딩 제거
+  keybindingRemoveAtUmount();
 });
 </script>
 
 <template>
-    <q-card-section id="unit" class="">
-      <q-field
-        :model-value="unitResult"
-        class="shadow-4 justify-end self-center"
-        filled
-        dense
-        readonly
-        :bg-color="
-          needUnitResultTooltip
-            ? store.darkMode
-              ? 'blue-grey-9'
-              : 'amber-2'
-            : store.darkMode
-              ? 'blue-grey-8'
-              : 'grey-2'
-        "
-      >
-        <template v-slot:control>
-          <div
-            id="unitResult"
-            v-mutation="setNeedUnitResultTooltip"
-            v-mutation.characterData
-            class="self-center full-width no-outline ellipsis text-h4 text-right"
-          >
-            {{ unitResult }}
-            <MyTooltip v-if="needUnitResultTooltip">{{ unitResult }}</MyTooltip>
-          </div>
-        </template>
-      </q-field>
-    </q-card-section>
+  <q-dialog
+    v-model="store.unitPanel"
+    style="z-index: 5"
+    seamless
+    transition-show="slide-down"
+    transition-hide="slide-up"
+  >
+    <q-card id="unit" class="full-width q-px-none q-mx-sm" v-blur>
+      <q-card-section class="row q-pt-sm">
+        <q-select
+          v-model="store.recentUnitFrom[store.recentCategory]"
+          :options="UnitConverter.getUnitLists(store.recentCategory)"
+          dense
+          options-dense
+          filled
+          class="col-4 shadow-4"
+        >
+          <template v-slot:before>
+            <q-icon name="arrow_upward" class="q-ml-sm" @click.stop.prevent />
+          </template>
+        </q-select>
+        <q-select
+          v-model="store.recentCategory"
+          :options="UnitConverter.categories"
+          dense
+          options-dense
+          filled
+          class="col-4 shadow-4 text-no-wrap"
+        >
+          <template v-slot:after>
+            <q-icon
+              name="swap_vert"
+              class="q-mr-sm cursor-pointer"
+              @click="swapUnitValue"
+              @click.stop.prevent
+            />
+          </template>
+        </q-select>
+        <q-select
+          v-model="store.recentUnitTo[store.recentCategory]"
+          :options="UnitConverter.getUnitLists(store.recentCategory)"
+          dense
+          options-dense
+          filled
+          class="col-4 shadow-4"
+        >
+          <template v-slot:after>
+            <q-icon name="arrow_downward" class="q-mr-sm" @click.stop.prevent />
+          </template>
+        </q-select>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none q-pb-md">
+        <q-field
+          :model-value="unitResult"
+          class="shadow-4 justify-end self-center"
+          filled
+          dense
+          readonly
+          :bg-color="
+            needUnitResultTooltip
+              ? store.darkMode
+                ? 'blue-grey-9'
+                : 'amber-2'
+              : undefined
+          "
+        >
+          <template v-slot:control>
+            <div
+              id="unitResult"
+              v-mutation="setNeedUnitResultTooltip"
+              v-mutation.characterData
+              class="self-center full-width no-outline ellipsis text-h4 text-right"
+            >
+              {{ unitResult }}
+              <MyTooltip v-if="needUnitResultTooltip">{{
+                unitResult
+              }}</MyTooltip>
+            </div>
+          </template>
+        </q-field>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
 </template>
 
 <style scoped lang="scss">
 #unit {
-  max-width: calc(100vw - 12px);
+  max-width: calc(100vw - 15px);
   position: fixed;
-  top: 150px;
+  top: 140px;
 }
 </style>
