d47ad2a6ea41ff69e2f71b8578cfe6414d7eea24
Seo Kihyun <from104@gmail.com>
2022-12-25 22:41:21 +0900

    베이스 데이터에 시간 추가 베이스 데이터를 직관적으로 기본 단위 배수로 수정 (기존에는 역수)

--- src/classes/UnitConverter.ts
+++ src/classes/UnitConverter.ts
@@ -8,49 +8,49 @@ type UnitBaseData = Record<string, Unit>;
 const unitBaseData: UnitBaseData = {
   length: {
     m: 1,
-    km: 1e-3,
-    cm: 100,
-    mm: 1000,
-    in: 39.37007874015748,
-    ft: 3.280839895013123,
-    yd: 1.0936132983377078,
-    mi: 0.000621371192237334,
+    km: 1000,
+    cm: 0.01,
+    mm: 0.001,
+    in: 0.0254,
+    ft: 0.3048,
+    yd: 0.9144,
+    mi: 1609.344,
   },
   area: {
     'm²': 1,
-    'km²': 1e-6,
-    'cm²': 10000,
-    'mm²': 1e6,
-    'in²': 1550.0031000062,
-    'ft²': 10.763910416709722,
-    'yd²': 1.19599004630108,
-    'mi²': 3.86102158542446e-7,
-    py: 0.3025,
-    ha: 1e-4,
-    a: 1e-2,
-    ac: 2.471053814671653e-4,
+    'km²': 1000000,
+    'cm²': 0.0001,
+    'mm²': 0.000001,
+    'in²': 0.00064516,
+    'ft²': 0.09290304,
+    'yd²': 0.83612736,
+    'mi²': 2589988.110336,
+    ha: 10000,
+    a: 100,
+    ac: 4046.8564224,
+    py: 3.3025,
   },
   volume: {
     'm³': 1,
-    'km³': 1e-9,
-    'cm³': 1e6,
-    'mm³': 1e9,
-    'in³': 61023.74409473228,
-    'ft³': 35.31466672148859,
-    'yd³': 1.307950619314392,
-    'mi³': 2.399127586206896e-10,
-    l: 1000,
-    ml: 1e6,
-    kl: 1,
-    gal: 264.1720523581484,
+    'km³': 1000000000,
+    'cm³': 0.000001,
+    'mm³': 0.000000001,
+    'in³': 0.000016387064,
+    'ft³': 0.028316846592,
+    'yd³': 0.764554857984,
+    'mi³': 4168181825.44058,
+    l: 0.001,
+    ml: 0.000001,
+    kl: 1000,
+    gal: 0.003785411784,
   },
   weight: {
-    kg: 1,
-    g: 1000,
-    mg: 1e6,
-    oz: 35.27396194958041,
-    lb: 2.2046226218487757,
-    ton: 0.001,
+    g: 1,
+    kg: 1000,
+    mg: 0.001,
+    oz: 28.349523125,
+    lb: 453.59237,
+    ton: 1000000,
   },
   temp: {
     C: 1,
@@ -59,29 +59,38 @@ const unitBaseData: UnitBaseData = {
     R: (t: number, r = false) =>
       r ? (t - 491.67) * (5 / 9) : t * (9 / 5) + 491.67,
   },
+  time: {
+    sec: 1,
+    min: 60,
+    hour: 3600,
+    day: 86400,
+    week: 604800,
+    month: 2628000,
+    year: 31536000,
+  },
   speed: {
     'km/h': 1,
-    'm/s': 0.277778,
-    'ft/s': 0.91134514435653324327,
-    'mi/h': 0.6213716893348046,
-    knot: 0.53995723588740196508,
+    'm/s': 3.6,
+    'ft/s': 1.09728,
+    'mi/h': 1.609344,
+    knot: 1.852,
   },
   pressure: {
     Pa: 1,
-    kPa: 0.001,
-    MPa: 1e-6,
-    hPa: 0.01,
-    bar: 0.00001,
-    psi: 0.0001450377377302092,
-    ksi: 1.450377377302092e-7,
+    kPa: 1000,
+    MPa: 1000000,
+    hPa: 100,
+    bar: 100000,
+    psi: 6894.75729316836,
+    ksi: 6894757.29316836,
   },
   bytes: {
-    B: 1125899906842624,
-    KB: 1099511627776,
-    MB: 1073741824,
-    GB: 1048576,
-    TB: 1024,
-    PB: 1,
+    B: 1,
+    KB: 1024,
+    MB: 1048576,
+    GB: 1073741824,
+    TB: 1099511627776,
+    PB: 1125899906842624,
   },
 };
 
@@ -106,9 +115,7 @@ class UnitConverter {
     return Object.keys(this.units[category]);
   }
 
-  // 단위 변환 범주 목록별 단위 목록에서
-  // 단위를 찾아서 반환 typescript
-
+  // 단위를 찾아서 반환
   static getUnit(category: string, unit: string) {
     // 예외 처리
     if (!this.categories.includes(category)) {
@@ -142,7 +149,7 @@ class UnitConverter {
     if (typeof fromUnitInfo === 'function') {
       baseValue = fromUnitInfo(value, true);
     } else {
-      baseValue /= fromUnitInfo;
+      baseValue *= fromUnitInfo;
     }
 
     // 기준 단위에서 toUnit으로 값 변환
@@ -150,7 +157,7 @@ class UnitConverter {
     if (typeof toUnitInfo === 'function') {
       convertedValue = toUnitInfo(convertedValue);
     } else {
-      convertedValue *= toUnitInfo;
+      convertedValue /= toUnitInfo;
     }
 
     return convertedValue;
