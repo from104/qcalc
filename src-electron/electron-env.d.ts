7cf8a5fc696940bde74e31dc607d080c37ebb10e
Seo Kihyun <from104@gmail.com>
2024-05-27 01:25:11 +0900

    Update NodeJS process environment declarations

--- src-electron/electron-env.d.ts
+++ src-electron/electron-env.d.ts
@@ -1,12 +1,10 @@
 /* eslint-disable */
 
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-        QUASAR_PUBLIC_FOLDER: string;
-        QUASAR_ELECTRON_PRELOAD_FOLDER: string;
-        QUASAR_ELECTRON_PRELOAD_EXTENSION: string;
-        APP_URL: string;
-    }
+declare module NodeJS {
+  interface ProcessEnv {
+      QUASAR_PUBLIC_FOLDER: string;
+      QUASAR_ELECTRON_PRELOAD_FOLDER: string;
+      QUASAR_ELECTRON_PRELOAD_EXTENSION: string;
+      APP_URL: string;
   }
 }
