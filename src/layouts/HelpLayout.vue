62df437c066270cac30988eb219131a72a91dd97
Seo Kihyun <from104@gmail.com>
2024-06-02 22:38:51 +0900

    refactor: Add HelpLayout.vue and CalcLayout.vue for better code organization

---
+++ src/layouts/HelpLayout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HelpPage from 'src/pages/HelpPage.vue';
+
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="z-top noselect" elevated>
+      <q-toolbar v-blur>
+        <q-btn
+          flat
+          dense
+          round
+          icon="back"
+          :aria-label="t('message.help')"
+        />
+        <q-toolbar-title>{{ t('message.help') }}</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container style="padding-bottom: 0px;">
+      <HelpPage />
+    </q-page-container>
+  </q-layout>
+</template>
