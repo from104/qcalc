10abb96b162d1dd2a7e02c0d9dd0b9ebf9ef1f98
Seo Kihyun <from104@gmail.com>
2024-06-02 22:40:02 +0900

    refactor: Update routes for better code organization and layout separation

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,21 +1,17 @@
 import { RouteRecordRaw } from 'vue-router';
-// import { useCalcStore } from 'src/stores/calc-store';
-// const store = useCalcStore();
-// const initialPath = store.initPanel ? '/calc' : store.initialPath;
-const initialPath = '/calc';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', redirect: initialPath },
-      { path: '/calc', component: () => import('pages/CalcPage.vue') },
-      { path: '/unit', component: () => import('pages/UnitPage.vue') },
-      { path: '/currency', component: () => import('pages/CurrencyPage.vue') },
-      { path: '/help', component: () => import('pages/HelpPage.vue') },
-      { path: '/about', component: () => import('pages/AboutPage.vue') },
-    ],
+    component: () => import('layouts/CalcLayout.vue'),
+  },
+  {
+    path: '/help',
+    component: () => import('layouts/HelpLayout.vue'),
+  },
+  {
+    path: '/about',
+    component: () => import('layouts/AboutLayout.vue'),
   },
 
   // Always leave this as last one,
