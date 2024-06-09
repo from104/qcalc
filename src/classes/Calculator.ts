a93e576394a5bf83189585357a6cfab27d742c50
Seo Kihyun <from104@gmail.com>
2024-06-10 01:32:31 +0900

    refactor: Increase maximum history size to 100 and add memo functionality

--- src/classes/Calculator.ts
+++ src/classes/Calculator.ts
@@ -1,4 +1,4 @@
-import { create, all } from 'mathjs';
+import { create, all, re } from 'mathjs';
 
 const MathB = create(all, {
   number: 'BigNumber',
@@ -36,6 +36,7 @@ interface History {
   operator: string;
   argumentNumber?: string;
   resultNumber: string;
+  memo?: string;
 }
 
 export class Calculator {
@@ -55,10 +56,10 @@ export class Calculator {
   private shouldReset!: boolean;
 
   // 계산 히스토리 저장 배열
-  private history: History[] = [];
+  private histories: History[] = [];
 
   // 히스토리 최대 크기
-  private readonly HISTORY_MAX_SIZE = 20;
+  private readonly HISTORY_MAX_SIZE = 100;
 
   // 생성자
   constructor() {
@@ -334,7 +335,7 @@ export class Calculator {
     if (this.currentOperator == Operator.Div || this.currentOperator == Operator.Mul) {
       this.performPreCalculation(); // 사전 계산
 
-      const { previousNumber, argumentNumber } = this.history.shift() as History; // 계산 결과를 빼냄
+      const { previousNumber, argumentNumber } = this.histories.shift() as History; // 계산 결과를 빼냄
       const operator =
         this.getOperatorString() + this.getOperatorString(Operator.Percent); // 연산자를 %로
       const resultNumber =
@@ -404,29 +405,81 @@ export class Calculator {
   // 히스토리 추가
   private addHistory(history: History): string {
     // console.log(history);
-    if (this.history.length == 0) {
+    if (this.histories.length == 0) {
       history.id = 1;
     } else {
-      history.id = Math.max(...(this.history.map((h) => h.id) as number[])) + 1;
+      history.id = Math.max(...(this.histories.map((h) => h.id) as number[])) + 1;
     }
-    // 배열 앞에 히스토리 추가
-    this.history.unshift(history);
-
+    // 배열 앞에 히스토리 추가ㄴ
+    this.histories.unshift(history);
+    
     // 히스토리 크기가 최대 크기를 넘어서면 제일 뒤의 것을 제거
-    if (this.history.length > this.HISTORY_MAX_SIZE) this.history.pop();
+    if (this.histories.length > this.HISTORY_MAX_SIZE) this.histories.pop();
 
     // 최종적으로 계산 결과를 반환
     return history.resultNumber;
   }
 
-  // 히스토리 얻기
-  public getHistory(idx = -1): History[] | History | undefined {
-    return idx == -1 ? this.history : this.history[idx];
+  // 히스토리 크기 얻기
+  public getHistorySize(): number {
+    return this.histories.length;
+  }
+
+  // 히스토리 전체 얻기
+  public getHistories(): History[] {
+    return this.histories;
+  }
+
+  // 히스토리의 id로 인덱스 찾기
+  public getHistoryIndexByID(id: number): number {
+    const index = this.histories.findIndex((h) => h.id === id);
+    if (index !== -1) {
+      return index;
+    } else {
+      throw new Error('History not found');
+    }
+  }
+
+  // 히스토리에서 배열 인덱스로 찾기
+  public getHistoryByIndex(index: number): History {
+    if (index >= 0 && index < this.histories.length) {
+      return this.histories[index];
+    } else {
+      throw new Error('History not found');
+    }
+  }
+
+  // 히스토리에서 id로 찾기
+  public getHistoryByID(id: number): History {
+    return this.getHistoryByIndex(this.getHistoryIndexByID(id));
+  }
+
+  // id를 사용하여 히스토리 배열에서 해당 항목을 찾아서 삭제
+  public deleteHistory(id: number): void {
+    this.histories.splice(this.getHistoryIndexByID(id), 1);
   }
 
   // 히스토리 초기화
   public clearHistory(): void {
-    this.history = [];
+    this.histories = [];
+  }
+
+  // 히스토리에 메모 셋팅
+  public setMemo(id: number, memo: string): void {
+    const index = this.getHistoryIndexByID(id);
+    this.histories[index].memo = memo;
+  }
+
+  // 히스토리에서 메모 얻기
+  public getMemo(id: number): string | null {
+    const index = this.getHistoryIndexByID(id);
+    return this.histories[index].memo || null;
+  }
+
+  // 히스토리에서 메모 삭제
+  public deleteMemo(id: number): void {
+    const index = this.getHistoryIndexByID(id);
+    delete this.histories[index].memo;
   }
 }
 
