f5ea6d803e74525fabcb6f2af60d0ba73b1ae4f7
Seo Kihyun <from104@gmail.com>
2024-06-13 23:55:21 +0900

    코드 정리

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n';
-import { useRouter } from 'vue-router';
-
-import { useMeta, useQuasar } from 'quasar';
-import { ref, watch, onBeforeMount } from 'vue';
+import { 
+  ref, 
+  watch, 
+  onBeforeMount 
+} from 'vue';
 
 import { useCalcStore } from 'stores/calc-store';
 
-const router = useRouter();
-
 const store = useCalcStore();
 
+import { useI18n } from 'vue-i18n';
 const { locale } = useI18n({ useScope: 'global' });
-
 const { t } = useI18n();
 
+import { useMeta, useQuasar } from 'quasar';
+useMeta(() => ({
+  title: title.value,
+}));
 const $q = useQuasar();
 
 const title = ref(t('message.appTitle'));
@@ -23,31 +25,33 @@ const updateTitle = () => {
   title.value = t('message.appTitle');
 };
 
-if ( $q.platform.is.electron ) {
-  store.setAlwaysOnTop( store.alwaysOnTop );
-}
-
-useMeta(() => ({
-  title: title.value,
-}));
-
 watch(() => store.locale, () => {
   updateTitle();
 });
 
-onBeforeMount( () => {
+onBeforeMount(() => {
   locale.value = store.locale;
 
-  if ( $q.platform.is.win ) {
+  if ($q.platform.is.win) {
     store.paddingOnResult = 8;
-  } else if ( $q.platform.is.linux ) {
+  } else if ($q.platform.is.linux) {
     store.paddingOnResult = 3;
   } else {
     store.paddingOnResult = 0;
   }
+
   updateTitle();
+
   store.isHistoryDialogOpen = false;
   store.isSettingDialogOpen = false;
+
+  if (store.initPanel) {
+    store.calc.clear();
+  }
+
+  if ( $q.platform.is.electron ) {
+    store.setAlwaysOnTop( store.alwaysOnTop );
+  }
 });
 </script>
 
