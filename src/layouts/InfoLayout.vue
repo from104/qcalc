1f573e40193bd4d8e948ed7460fa9a3ea1f03dc3
Seo Kihyun <from104@gmail.com>
2024-06-15 15:20:25 +0900

    - Calculate dynamic header height with computed property

--- src/layouts/InfoLayout.vue
+++ src/layouts/InfoLayout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted, onUnmounted, computed } from 'vue';
 
 import HelpPage from 'src/pages/HelpPage.vue';
 import AboutPage from 'src/pages/AboutPage.vue';
@@ -27,12 +27,20 @@ onMounted(() => {
 onUnmounted(() => {
   window.removeEventListener('keydown', goBackOnEsc);
 });
+
+const headerHeight = computed(() => {
+  const header = document.getElementById('header');
+  if (header) {
+    console.log(header.clientHeight);
+  }
+  return header ? header.clientHeight + 'px' : '0px';
+});
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header class="z-top noselect" elevated>
-      <q-toolbar id="header" v-blur>
+    <q-header id="header" class="z-top noselect" elevated>
+      <q-toolbar v-blur>
         <q-btn
           flat
           dense
@@ -44,9 +52,15 @@ onUnmounted(() => {
       </q-toolbar>
     </q-header>
     <q-page-container style="padding-bottom: 0px;">
-      <q-scroll-area style="height: calc(100vh - 50px)">
+      <q-scroll-area id="area">
         <component :is="Component" />
       </q-scroll-area>
     </q-page-container>
   </q-layout>
 </template>
+
+<style scoped lang="scss">
+#area {
+  height: calc(100vh - v-bind('headerHeight')) !important;
+}
+</style>
